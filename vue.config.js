542fd50d0689743d4a1577ed3695d87c821d658a
yugandco <ernarumbetali@yandex.ru>
2020-11-07 01:32:12 +0600

    FronSite

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 const path = require('path')
 
 module.exports = {
-    outputDir: path.resolve(__dirname, '../public'),
+    outputDir: path.resolve(__dirname, '../server/dist'),
     devServer: {
-        proxy: 'http://localhost:80'
+        proxy: {
+          '/api': {
+            target: 'http://localhost:80'
+          }
+        }
       }
 }
